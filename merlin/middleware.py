85f3d6b6c0b89d7cc9616095e6e153bc24f3f01f
Joe <joe.wesch@networktocode.com>
2021-04-16 02:46:28 +0000

    Disables pylint errors

--- merlin/middleware.py
+++ merlin/middleware.py
@@ -14,7 +14,7 @@ class Prerequisites:
         response = self.get_request(request)
         return response
 
-    def process_view(self, request, view_func, view_args, view_kwargs):
+    def process_view(self, request, view_func, view_args, view_kwargs):  # pylint: disable=W0613,R0201
         """Process the view to determine if it's one we want to intercept."""
         # If no user or they aren't authenticated, return to hit the default login redirect
         if not hasattr(request, "user") or not request.user.is_authenticated:
@@ -28,5 +28,5 @@ class Prerequisites:
                         if hasattr(base_fields[field], "queryset") and not base_fields[field].queryset:
                             name = field.replace("_", " ").title()
                             messages.error(request, f"You need to configure a {name} before you create this item.")
-            except Exception as error:
+            except Exception as error:  # pylint: disable=broad-except
                 print(error)
