2a9418561309b7c0bc67949161031101899ee0b2
Josh VanDeraa <jv@networktocode.com>
2021-04-22 11:52:32 -0500

    Black for migrations

--- merlin/migrations/0004_auto_20210422_1649.py
+++ merlin/migrations/0004_auto_20210422_1649.py
@@ -6,78 +6,78 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('merlin', '0003_merlin_sites'),
+        ("merlin", "0003_merlin_sites"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='merlin',
-            name='all_completed',
+            model_name="merlin",
+            name="all_completed",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='circuit_types',
+            model_name="merlin",
+            name="circuit_types",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='cluster_types',
+            model_name="merlin",
+            name="cluster_types",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='device_roles',
+            model_name="merlin",
+            name="device_roles",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='device_types',
+            model_name="merlin",
+            name="device_types",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='manufacturers',
+            model_name="merlin",
+            name="manufacturers",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='platforms',
+            model_name="merlin",
+            name="platforms",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='providers',
+            model_name="merlin",
+            name="providers",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='rirs',
+            model_name="merlin",
+            name="rirs",
         ),
         migrations.RemoveField(
-            model_name='merlin',
-            name='sites',
+            model_name="merlin",
+            name="sites",
         ),
         migrations.AddField(
-            model_name='merlin',
-            name='completed',
+            model_name="merlin",
+            name="completed",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='merlin',
-            name='ignored',
+            model_name="merlin",
+            name="ignored",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='merlin',
-            name='merlin_link',
-            field=models.CharField(default='', max_length=200),
+            model_name="merlin",
+            name="merlin_link",
+            field=models.CharField(default="", max_length=200),
         ),
         migrations.AddField(
-            model_name='merlin',
-            name='name',
-            field=models.CharField(default='', max_length=100),
+            model_name="merlin",
+            name="name",
+            field=models.CharField(default="", max_length=100),
         ),
         migrations.AddField(
-            model_name='merlin',
-            name='nautobot_add_link',
-            field=models.CharField(default='', max_length=48),
+            model_name="merlin",
+            name="nautobot_add_link",
+            field=models.CharField(default="", max_length=48),
         ),
         migrations.AddField(
-            model_name='merlin',
-            name='nautobot_model',
-            field=models.CharField(default='', max_length=48),
+            model_name="merlin",
+            name="nautobot_model",
+            field=models.CharField(default="", max_length=48),
         ),
     ]
