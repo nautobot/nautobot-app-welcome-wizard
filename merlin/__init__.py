005ff494e9df610dc80dd07bf6691b5b27187df7
Stephen Kiely <stephenkiely87@gmail.com>
2021-04-22 17:12:52 -0500

    Thanks Glenn!!!

--- merlin/__init__.py
+++ merlin/__init__.py
@@ -2,10 +2,10 @@
 
 __version__ = "0.1.0"
 
-# from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate
 from nautobot.extras.plugins import PluginConfig
 
-# from merlin.signals import merlin_devicetype
+from merlin.signals import merlin_devicetype
 
 
 class MerlinConfig(PluginConfig):
@@ -26,11 +26,11 @@ class MerlinConfig(PluginConfig):
     caching_config = {}
     middleware = ["merlin.middleware.Prerequisites"]
 
-    # def ready(self):
-    #     """If enable_devicetype-library is true, create the Git Repository."""
-    #     super().ready()
+    def ready(self):
+        """If enable_devicetype-library is true, create the Git Repository."""
+        super().ready()
 
-    # post_migrate.connect(merlin_devicetype, sender=self)
+        post_migrate.connect(merlin_devicetype, sender=self)
 
 
 config = MerlinConfig  # pylint:disable=invalid-name
--- merlin/signals.py
+++ merlin/signals.py
@@ -7,17 +7,12 @@ def merlin_devicetype(apps, **kwargs):
     """Add a git repository if enabled in settings."""
     git_repository = apps.get_model("extras", "GitRepository")
     if settings.PLUGINS_CONFIG["merlin"].get("enable_devicetype-library"):
-        try:
-            repo = git_repository.objects.get(name="Devicetype-library")
-        except git_repository.DoesNotExist:
-            repo = git_repository(
-                name="Devicetype-library",
-                slug="devicetype_library",
-                remote_url="https://github.com/netbox-community/devicetype-library.git",
-                provided_contents=[
-                    "merlin.device_type_importer",
-                ],
-                branch="master",
-            )
-            print(repo)
-            repo.save(trigger_resync=False)
+        git_repository.objects.update_or_create(
+            name="Devicetype-library",
+            slug="devicetype_library",
+            remote_url="https://github.com/netbox-community/devicetype-library.git",
+            provided_contents=[
+                "merlin.import_wizard",
+            ],
+            branch="master",
+        )
