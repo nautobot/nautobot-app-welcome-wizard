97c6e472a08defbdc760c95c56e1f8332f17493f
Jan Snasel <jan.snasel@networktocode.com>
2024-03-19 05:46:12 +0000

    chore: Manual fixes

--- welcome_wizard/__init__.py
+++ welcome_wizard/__init__.py
@@ -12,8 +12,8 @@ class WelcomeWizardConfig(NautobotAppConfig):
     verbose_name = "Nautobot Welcome Wizard"
     version = __version__
     author = "Network to Code, LLC"
-    description = "Welcome Wizard."
-    base_url = "welcome-wizard"
+    description = "Nautobot's Getting Started Wizard."
+    base_url = "welcome_wizard"
     required_settings = []
     min_version = "2.0.0"
     max_version = "2.9999"
