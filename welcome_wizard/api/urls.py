da03b17182aee2cc26f51f99744bd16c89724d57
Stephen Kiely <stephenkiely87@gmail.com>
2025-01-24 14:08:25 -0600

    Revert removal of app_name

--- welcome_wizard/api/urls.py
+++ welcome_wizard/api/urls.py
@@ -4,6 +4,7 @@ from nautobot.apps.api import OrderedDefaultRouter
 
 from welcome_wizard.api import views
 
+app_name = "welcome_wizard-api"
 router = OrderedDefaultRouter()
 # add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
 router.register("manufacturerimport", views.ManufacturerImportViewSet)
