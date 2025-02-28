3ce869ca2ca8fe0442ea728f6196135942cdb8d0
Stephen Kiely <stephenkiely87@gmail.com>
2025-02-28 07:50:01 -0600

    Add Module Bay and Admin support

--- welcome_wizard/admin.py
+++ welcome_wizard/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 
+from welcome_wizard.models.importer import ManufacturerImport, DeviceTypeImport
 from welcome_wizard.models.merlin import Merlin
 
 
@@ -10,3 +11,20 @@ class MerlinAdmin(admin.ModelAdmin):
     """Admin panel setup."""
 
     list_display = ("name", "completed", "ignored", "nautobot_model", "nautobot_add_link", "merlin_link")
+
+
+@admin.register(ManufacturerImport)
+class ManufacturerImportAdmin(admin.ModelAdmin):
+    """Admin panel setup."""
+
+    list_display = ("name",)
+
+
+@admin.register(DeviceTypeImport)
+class DeviceTypeImportAdmin(admin.ModelAdmin):
+    """Admin panel setup."""
+
+    list_display = ("name", "filename", "manufacturer")
+    list_filter = ("manufacturer",)
+    search_fields = ("name",)
+    ordering = ("manufacturer", "name")
